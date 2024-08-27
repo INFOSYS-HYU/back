104753a2f837b8315aeafe0d58eecb6163fb7a82
hong3579 <96526301+hong3579@users.noreply.github.com>
2024-08-27 16:22:31 +0900

    commit

--- routes/noticeRoute.js
+++ routes/noticeRoute.js
@@ -55,8 +55,7 @@ router.post('/', upload.array("img1", 10), async (req, res) => {
     const newNotice = await createNotice(title, content);
      // 공지사항에 관련된 파일 정보 저장
     if (files.length > 0) {
-      const fileUrls = files.map(file => file.location);
-      await saveNoticeImages(newNotice.id, fileUrls);
+      await saveNoticeImages(newNotice.id, files);
     }
 
     res.status(201).json(newNotice);
