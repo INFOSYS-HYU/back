ff90f76a082b15ee2c01aa0fa8e8556c28b8d7cb
sysys10 <166797095+sysys10@users.noreply.github.com>
2024-08-27 15:07:36 +0900

    commit multer

--- app.js
+++ app.js
@@ -1,32 +1,52 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const noticeRoute = require("./routes/noticeRoute");
-const financeRoute = require("./routes/financeRoute");
-const calendarRoute = require("./routes/calendarRoute");
-const galleryRoute = require("./routes/galleryRoute");
-const authStudent = require("./auth");
+const { S3Client } = require("@aws-sdk/client-s3");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+require("dotenv").config();
+
+const noticeRoutes = require('./routes/noticeRoute');
+const financeRoutes = require('./routes/financeRoute');
+const calendarRoutes = require('./routes/calendarRoute');
+const galleryRoutes = require('./routes/galleryRoute');
+const authRoutes = require('./routes/authRoute');
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
-// Routes
-app.use("/api/notice", noticeRoute);
-app.use("/api/finance", financeRoute);
-app.use("/api/calendar", calendarRoute);
-app.use("/api/gallery", galleryRoute);
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
+const s3 = new S3Client({
+  region: "ap-southeast-2",
+  credentials: {
+    accessKeyId: process.env.KeyId,
+    secretAccessKey: process.env.Secretkey,
+  },
 });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: "infosysweb",
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString());
+    },
+  }),
+});
+
+app.post("/add", upload.single("img1"), async (req, res) => {
+  console.log(req.file.location);
+  res.send("File uploaded successfully");
+});
+
+// Routes
+app.use('/api/notice', noticeRoutes);
+app.use('/api/finance', financeRoutes);
+app.use('/api/calendar', calendarRoutes);
+app.use('/api/gallery', galleryRoutes);
+
+app.listen(3001, () => {
+  console.log("Server is running on http://localhost:3001");
 });
