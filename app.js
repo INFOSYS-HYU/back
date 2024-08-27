c99c819b3cc010263f7ef30f884b80466617851d
hong3579 <96526301+hong3579@users.noreply.github.com>
2024-08-27 16:04:47 +0900

    commit

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const noticeRoutes = require('./routes/noticeRoute');
 const financeRoutes = require('./routes/financeRoute');
 const calendarRoutes = require('./routes/calendarRoute');
 const galleryRoutes = require('./routes/galleryRoute');
-const authRoutes = require('./routes/authRoute');
 
 const app = express();
 
@@ -35,19 +34,6 @@ const upload = multer({
     },
   }),
 });
-
-export const uploadImg = (req, res) => {
-  return new Promise((resolve, reject) => {
-    upload.array('images', 10)(req, res, (err) => {
-      if (err) {
-        reject(err);  // 에러 발생 시 reject
-      } else {
-        const imageUrls = req.files.map(file => file.location);  // 업로드된 파일의 URL 가져오기
-        resolve(imageUrls);  // 성공 시 resolve
-      }
-    });
-  });
-};
 
 
 app.post("/add", upload.single("img1"), async (req, res) => {
--- routes/noticeRoute.js
+++ routes/noticeRoute.js
@@ -47,12 +47,17 @@ router.get('/detail/:id', async (req, res) => {
 });
 
 // 공지사항 생성
-router.post('/', async (req, res) => {
-  const upimg = await uploadImg;
-  console.log(upimg);
+router.post('/', upload.array("img1", 10), async (req, res) => {
   const { title, content } = req.body;
+  const files = req.files; // 업로드된 파일들
+
   try {
     const newNotice = await createNotice(title, content);
+     // 공지사항에 관련된 파일 정보 저장
+    if (files.length > 0) {
+      await saveNoticeImages(newNotice.id, files);
+    }
+
     res.status(201).json(newNotice);
   } catch (error) {
     console.error("Error creating notice:", error);
